1f0b71fef2ee0d7593991bdd798c693b9f06e134
jkeppers <justin@keppers.org>
2015-02-17 14:09:34 -0600

    prod env config

--- config/connections.js
+++ config/connections.js
@@ -48,6 +48,8 @@ module.exports.connections = {
     database: 'YOUR_MYSQL_DB'
   },
 
+
+
   /***************************************************************************
   *                                                                          *
   * MongoDB is the leading NoSQL database.                                   *
@@ -56,13 +58,13 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
-  someMongodbServer: {
+  localMongodbServer: {
     adapter: 'sails-mongo',
     host: 'localhost',
     port: 27017,
     // user: 'username',
     // password: 'password',
-    // database: 'your_mongo_db_name_here'
+    database: 'taplist'
   },
 
   /***************************************************************************
--- config/env/production.js
+++ config/env/production.js
@@ -17,9 +17,9 @@ module.exports = {
    * environment (see config/connections.js and config/models.js )           *
    ***************************************************************************/
 
-  // models: {
-  //   connection: 'someMysqlServer'
-  // },
+   models: {
+     connection: 'localMongodbServer'
+   },
 
   /***************************************************************************
    * Set the port in the production environment to 80                        *
@@ -34,5 +34,5 @@ module.exports = {
   // log: {
   //   level: "silent"
   // }
-
+  sails.config.models.migrate="safe"
 };
